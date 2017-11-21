8d1e12e9875bcc499f5b409cfc97407df51e47fc
nadavperetz <nadavperetz@gmail.com>
2017-11-21 10:51:54 -0200

    typo

--- src/reducers/reducer.jsx
+++ src/reducers/reducer.jsx
@@ -96,7 +96,7 @@ function CommentStore(state = initialCommentState, action) {
         comments: action.comments
       }
     case UPDATE_COMMENT:
-      gilet comments = state.comments.map(a => ({...a}));
+      let comments = state.comments.map(a => ({...a}));
       let idx = comments.findIndex((comment) => comment.id === action.comment.id)
       if (idx >= 0)
         if (!action.comment.deleted)
